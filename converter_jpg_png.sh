f4ed3eed3f7f6fa529c780820dc836fedb61ee66
Fernando Tulio <fernandotulio@gmail.com>
2022-09-23 15:17:36 -0300

    Ajuste script de conversao todos os arquivos, ajsute

--- converter_jpg_png.sh
+++ converter_jpg_png.sh
@@ -2,12 +2,18 @@
 
 cd ~/Documentos/imagens-livros
 
+# -d e diretorio, se nao existe cria
+if [ ! -d png ]
+then
+    mkdir png
+fi
+
 for imagem in *.jpg
 do
-  echo 'Converting '$imagem
-   imagem_sem_extensao=$(ls $imagem | awk -F. '{print $1}')
-   convert $imagem_sem_extensao.jpg $imagem_sem_extensao.png
+    echo 'Converting '$imagem
+    imagem_sem_extensao=$(ls $imagem | awk -F. '{ print $1 }')
+    convert $imagem_sem_extensao.jpg png/$imagem_sem_extensao.png
 done
 
-
+ 
  
